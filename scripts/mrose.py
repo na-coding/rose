3d45183f4d868ee6acbd1f93381b0090b3c4b6b3
Nigel <npb.aoyang@gmail.com>
2019-10-17 10:51:04 +0800

    Completed Maurer Rose
    - Changed sin and cos params to radians

--- scripts/mrose.py
+++ scripts/mrose.py
@@ -1,22 +1,32 @@
 import turtle
-from math import sin, cos, pi
+from math import sin, cos, radians
 
 screen = turtle.Screen()
 screen.tracer(0)
 
 turtle.hideturtle()
 turtle.speed(0)
+turtle.color('black', 'red')
 
-n = 5
-distance = 100
+n = 6
+d = 71
+distance = 150
 
-for i in range (0, 361, 1):
-    x = distance * cos(n*i) * cos(i)
-    y = distance * cos(n*i) * sin(i)
-    turtle.penup()
+turtle.penup()
+turtle.begin_fill()
+for i in range (361):
+    # x = distance * cos(radians(n*i)) * cos(radians(i))
+    # y = distance * cos(radians(n*i)) * sin(radians(i))
+
+    k = i * d
+    r = distance * sin(radians(n * k))
+    x =  r * cos(radians(k))
+    y = r * sin(radians(k))
+
     turtle.goto(x,y)
     turtle.pendown()
     turtle.dot(6)
     turtle.update()
 
+turtle.end_fill()
 turtle.exitonclick()
